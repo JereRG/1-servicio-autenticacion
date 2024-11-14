df15f43e601cd894a3c933b859461ac2e2aaa1be
JereRG <jeremiasrg02@gmail.com>
2024-11-14 14:45:42 -0300

    a

--- src/main/java/com/example/autenticacion/config/CorsConfig.java
+++ src/main/java/com/example/autenticacion/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package main.java.com.example.autenticacion.config;
+package com.example.autenticacion.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
